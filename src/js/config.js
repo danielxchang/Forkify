892ded7704a205be8ac207ea930079160ba11471
Daniel Chang <daniel.changk@gmail.com>
2022-02-10 17:33:23 -0600

    completed rest of app

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '3dc1e0ef-53aa-44c3-943a-b98d3ec1a05c';
+export const MODAL_CLOSE_SEC = 2.5;
