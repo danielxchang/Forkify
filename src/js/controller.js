f7d0a02b9bc7c6b358f1d7f3c2ccaf88acd3c419
Daniel Chang <daniel.changk@gmail.com>
2022-02-15 12:45:07 -0600

    Removing continuous integration test code

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,7 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('TESTING CONTINUOUS INTEGRATION');
 };
 
 init();
