7d4ecf426eec98666f33b6e5aa9a15141abf8ed6
dcphantom <daniel.changk@gmail.com>
2022-02-15 12:38:07 -0600

    testing continuous integration

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,6 +132,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('TESTING CONTINUOUS INTEGRATION');
 };
 
 init();
